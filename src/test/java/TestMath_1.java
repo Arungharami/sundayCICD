233bc9498bbc7f1eabece55687d1ef4ccdde66d8
Arun Gharami <64158561+Arungharami@users.noreply.github.com>
2024-11-09 11:08:07 -0800

    new test

--- src/test/java/TestMath_1.java
+++ src/test/java/TestMath_1.java
@@ -30,6 +30,22 @@ public class TestMath_1 {
         int returnedVlueFor19 = obj.Add2Numbers(-5 , 5);
         Assert.assertEquals(returnedVlue,30);
 
-        Assert.assertEquals(obj)
+        int returnedVlueFor19 = obj.Add2Numbers(-5, 5);
+
+        Assert.assertEquals(obj.Add2Number(-5, 2), -3);
+
+        Assert.assertEquals(obj.Add2Numbers(0, 0), 0);
+    }
+
+    @Test(priotity = 1)
+    puclic void TestAdditionWithDiffrentValues()
+    {
+        MathCalculator obj = new MathCalculator();
+
+        Assert.assertEquals(obj.DifferenceNumbers(10,8),2);
+
+        Assert.assertEquals(obj.DifferenceNumbers(0, 0), 0);
+
+        Assert.assertEquals(obj.DifferenceNumbers(3, 0), 3);
     }
 }
